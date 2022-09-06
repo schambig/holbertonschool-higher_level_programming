c2417ef480445b0d99e5423bc97d8c263fd16f5a
Salomon Chambi <salomonchambi@gmail.com>
2022-09-05 17:53:30 -0700

    Print different words usign the slicing notation

--- python-hello_world/7-edges.py
+++ python-hello_world/7-edges.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 word = "Holberton"
-# remember the lower bound is inclusive and the upper bound exclusive
 word_first_3 = word[0:3]
 word_last_2 = word[-2:]
-# word_last_2 = word[7:] this also works
 middle_word = word[1:-1]
 print(f"First 3 letters: {word_first_3}")
 print(f"Last 2 letters: {word_last_2}")
 print(f"Middle word: {middle_word}")
+# remember the lower bound is inclusive and the upper bound exclusive
+# word_last_2 = word[7:] this also works
