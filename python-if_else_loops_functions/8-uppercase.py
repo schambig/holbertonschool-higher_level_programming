35a99e9d2c07495c8a1f56c8bff70c33012151fb
Salomon Chambi <salomonchambi@gmail.com>
2022-09-11 01:40:50 -0700

    Print a string in uppercase followed by a new line

---
+++ python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def uppercase(str):
+    new = ''
+    for i in str:
+        j = ord(i) - 32
+        if 97 <= ord(i) <= 122:
+            new = new + chr(j)
+        else:
+            new = new + i
+    print(new)
