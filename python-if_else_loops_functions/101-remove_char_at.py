0f77e54717f5ab28aad99dacf2e9bddf7babbf78
Salomon Chambi <salomonchambi@gmail.com>
2022-09-11 13:28:27 -0700

    Copy a string, removing the character at the position `n`

--- python-if_else_loops_functions/101-remove_char_at.py
+++ python-if_else_loops_functions/101-remove_char_at.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 def remove_char_at(str, n):
-    if (n >= 0):
-        return str[:n] + str[n + 1:]
-    else:
-        return str
+    return str[:n] + str[n + 1:]  if (n >= 0) else str
+
+# The code below also works
+#   if (n >= 0):
+#       return str[:n] + str[n + 1:]
+#   else:
+#       return str
