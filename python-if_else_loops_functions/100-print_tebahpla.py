43ce1035ec7bbe7ece20d2faada5d8d325572b6e
Salomon Chambi <salomonchambi@gmail.com>
2022-09-11 12:43:17 -0700

    Print ASCII alphabet, in reverse, alternating lowercase and uppercase

--- python-if_else_loops_functions/100-print_tebahpla.py
+++ python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,3 +1,8 @@
 #!/usr/bin/python3
 for i in range(122, 96, -1):
     print("{:c}".format(i) if (i % 2 == 0) else "{:c}".format(i - 32), end='')
+
+# The code below also works
+#   print(f"{chr(i)}" if (i % 2 == 0) else f"{chr(i - 32)}", end='')
+# This is another method
+#   print(f"{chr(i) if (i % 2 == 0) else chr(i - 32)}", end='')
