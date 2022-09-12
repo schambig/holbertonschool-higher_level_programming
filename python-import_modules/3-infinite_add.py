1aefb95d4fbeb50e8e10d22a415716e744a41947
Salomon Chambi <salomonchambi@gmail.com>
2022-09-11 19:40:05 -0700

    Print the result of the addition of all arguments

--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -1,14 +1,20 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    # import the argv list from the sys module
     from sys import argv
     add = 0
-    len = len(argv)
-    if (len - 1 == 0):
-        print('0')
-    elif (len - 1 == 1):
-        print(argv[1])
-    else:
-        for i in range(1, len):
+    if (len(argv) > 1):
+        for i in range(1, len(argv)):
             add += int(argv[i])
-        print(add)
+    print(add)  # or print(f"{add}"), or print("{:d}".format(add))
+
+# The code below also works
+#    add = 0
+#    len = len(argv)
+#    if (len - 1 == 0):
+#        print('0')
+#    elif (len - 1 == 1):
+#        print(argv[1])
+#    else:
+#        for i in range(1, len):
+#            add += int(argv[i])
+#        print(add)
