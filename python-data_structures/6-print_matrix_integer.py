1116683f885f85522c36834439c3fffc1f81dff9
Salomon Chambi <salomonchambi@gmail.com>
2022-09-13 10:46:46 -0700

    Fix: consider case: matrix[[]]

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,10 +1,18 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    if not matrix:
-        return
-    for i in range(0, len(matrix)):
-        for j in range(0, len(matrix[i])):
-            if (j == len(matrix[i]) - 1):
-                print("{:d}".format(matrix[i][j]))
+    for row in matrix:
+        for i in row:
+            if i is not row[len(row) - 1]:
+                print("{:d}".format(i), end=' ')
             else:
-                print("{:d} ".format(matrix[i][j]), end='')
+                print("{:d}".format(i), end='')
+        print()
+
+# The code below also works
+#    for i in range(0, len(matrix)):
+#        for j in range(0, len(matrix[i])):
+#            if (j == len(matrix[i]) - 1):
+#                print("{:d}".format(matrix[i][j]), end='')
+#            else:
+#                print("{:d}".format(matrix[i][j]), end=' ')
+#        print(end='\n')
