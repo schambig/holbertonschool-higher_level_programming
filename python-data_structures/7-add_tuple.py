df9438e1ce79f44d89bbb8b1ff0b2eb154b16259
Salomon Chambi <salomonchambi@gmail.com>
2022-09-13 17:10:31 -0700

    Refactor: add two tuples

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -1,18 +1,20 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-    res = []
-    if (len(tuple_a) < 2):
-        if (len(tuple_a) == 0):
-            tuple_a = (0, 0)
-        else:
-            tuple_a = (tuple_a[0], 0)
-    if (len(tuple_b) < 2):
-        if (len(tuple_b) == 0):
-            tuple_b = (0, 0)
-        else:
-            tuple_b = (tuple_b[0], 0)
-    for i in range(0, 2):
-        res.append(tuple_a[i] + tuple_b[i])
-    res = tuple(res)
+    tuple_a += (0, 0)
+    tuple_b += (0, 0)
 
-    return res
+    return (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
+
+# The below code also works
+#    if (len(tuple_a) < 2):
+#        if (len(tuple_a) == 0):
+#            tuple_a = (0, 0)
+#        else:
+#            tuple_a = (tuple_a[0], 0)
+#    if (len(tuple_b) < 2):
+#        if (len(tuple_b) == 0):
+#            tuple_b = (0, 0)
+#        else:
+#            tuple_b = (tuple_b[0], 0)
+#
+#    return (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
