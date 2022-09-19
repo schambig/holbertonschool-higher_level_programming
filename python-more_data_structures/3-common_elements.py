454acea555bbae7e7c6f6052eaf9bdab56dfc041
Salomon Chambi <salomonchambi@gmail.com>
2022-09-18 19:00:06 -0700

    Return a set of common elements in two sets

---
+++ python-more_data_structures/3-common_elements.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def common_elements(set_1, set_2):
+    if set_1 is not None and set_2 is not None:
+        return(set_1 & set_2)
+    return
+
+# This works too:
+#   return set_1.intersection(set_2) in line 4
