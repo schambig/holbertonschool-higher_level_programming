0a55729fc20103dbb6221d7067fdd4b8e82428e8
Salomon Chambi <salomonchambi@gmail.com>
2022-09-24 22:00:11 -0700

    Add two integers

---
+++ python-test_driven_development/0-add_integer.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+Contains one method that returns an int sum
+Accepts two values, whether int or float, and casts them to int before adding
+"""
+
+
+def add_integer(a, b=98):
+    """
+    Returns a + b as int
+    """
+    if not isinstance(a, (int, float)):
+        raise TypeError("a must be an integer")
+    elif not isinstance(b, (int, float)):
+        raise TypeError("b must be an integer")
+    else:
+        return int(a) + int(b)
