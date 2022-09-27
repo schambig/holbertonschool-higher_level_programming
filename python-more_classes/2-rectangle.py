e43c57178de614be011010ed007c2e76b05537b6
Salomon Chambi <salomonchambi@gmail.com>
2022-09-26 21:31:15 -0700

    Defines a rectangle based on `1-rectangle.py` using `Rectangle`

--- python-more_classes/2-rectangle.py
+++ python-more_classes/2-rectangle.py
@@ -7,7 +7,7 @@ and public area and perimeter methods
 
 class Rectangle:
     """
-    Defines class rectangle with private attribute width and height
+    Defines a class rectangle with private attribute width and height
     Args:
         width (int): width
         height (int): height
