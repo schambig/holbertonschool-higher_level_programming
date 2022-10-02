332ec48bf6f4f3f132515f3bb8f2a8896f339ef9
Salomon Chambi <salomonchambi@gmail.com>
2022-10-01 19:40:09 -0700

    Write a class `MyList` that inherits from `list`

---
+++ python-inheritance/1-my_list.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+Contains class MyList
+inherits from list; has public instance method to print sorted
+"""
+
+
+class MyList(list):
+    """inherits from list
+    """
+    def print_sorted(self):
+        """prints list of ints all sorted in ascending order"""
+        print(sorted(self))
