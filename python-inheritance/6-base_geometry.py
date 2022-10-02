268e7f9e005771c5a6ab271b1596f52f54930933
Salomon Chambi <salomonchambi@gmail.com>
2022-10-01 21:48:25 -0700

    Create a class `BaseGeometry`, based on `5-base_geometry.py`

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+Contains empty class BaseGeometry
+with public instance method area
+"""
+
+
+class BaseGeometry:
+    """
+    Methods:
+        area(self)
+    """
+    def area(self):
+        """not implemented"""
+        raise Exception("area() is not implemented")
