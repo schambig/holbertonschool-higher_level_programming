3db36f3ca24a48f1b6d0d60494713535917c32b5
Salomon Chambi <salomonchambi@gmail.com>
2022-10-01 20:22:11 -0700

    Return `True` if `obj` is exactly an instance of the specified class

---
+++ python-inheritance/2-is_same_class.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+Contains method is_same_class
+returns True if object is exactly an instance of specified class
+"""
+
+
+def is_same_class(obj, a_class):
+    """
+    Notes:
+        use type() to get specific class
+        use isinstance() to get class and any parent classes too
+        use issubclass() to get what object is a subclass of
+    Return:
+        True if obj is exactly an instance of specified class
+    """
+    return type(obj) == a_class
