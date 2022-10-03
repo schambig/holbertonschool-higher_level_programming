7788227b6ff4d6f5a70463c360c19efeec146972
Salomon Chambi <salomonchambi@gmail.com>
2022-10-02 18:56:12 -0700

    Read a text file `UTF8` and print it to stdout

---
+++ python-input_output/0-read_file.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+Contains function that reads and prints contents from file
+"""
+
+
+def read_file(filename=""):
+    """Read and print text from file"""
+    with open(filename, mode="r", encoding="utf-8") as f:
+        print(f.read(), end="")
