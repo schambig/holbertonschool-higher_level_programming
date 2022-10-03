d4b1d88409dd6ffc57cf27a81f9f4aefa9e1492e
Salomon Chambi <salomonchambi@gmail.com>
2022-10-02 19:08:59 -0700

    Append a str (end) to a text file `UTF8`, return the number of chars added

---
+++ python-input_output/2-append_write.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Defines a file-appending function."""
+
+
+def append_write(filename="", text=""):
+    """appends to text file and returns num chars added"""
+    with open(filename, 'a', encoding="utf-8") as f:
+        return(f.write(text))
