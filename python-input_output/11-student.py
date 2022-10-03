01043ab575a1daca63675ad75e4afde9f6cc1eb0
Salomon Chambi <salomonchambi@gmail.com>
2022-10-02 21:13:56 -0700

    Write a class `Student` that defines a student based on `10-student.py`

---
+++ python-input_output/11-student.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+Contains class Student that initializes public instance attributes
+first_name, last_name, and age,and has public method to_json that returns
+dictionary representation of requested attributes or all if none were requested
+"""
+
+
+class Student:
+
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        if attrs is not None:
+            k = {}
+            for a in attrs:
+                if a in self.__dict__:
+                    k[a] = self.__dict__[a]
+            return k
+        return self.__dict__
+
+    def reload_from_json(self, json):
+        for a in json:
+            self.__dict__[a] = json[a]
