afdf94e2ffb2c66204d6e5923e4009f569171a69
Salomon Chambi <salomonchambi@gmail.com>
2022-10-02 20:17:54 -0700

    Add all arguments to a Python list, and then save them to a file

---
+++ python-input_output/7-add_item.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+This module defines a script which takes argvs
+and appends them to a python list, then writes
+the data in a new text file 'add_item.json' in
+JSON format.
+"""
+
+
+from sys import argv
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+
+filename = "add_item.json"
+
+try:
+    existing_content = load_from_json_file(filename)
+except FileNotFoundError:
+    existing_content = []
+
+save_to_json_file(existing_content + argv[1:], filename)
