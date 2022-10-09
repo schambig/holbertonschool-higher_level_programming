29663ac2103e636c00b4bdd956c0db49a71bfe48
Salomon Chambi <salomonchambi@gmail.com>
2022-10-09 10:35:00 -0700

    Define first class `Base`

---
+++ python-almost_a_circle/models/base.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+Module contains class Base
+
+Contains private class __nb_objects, and class constructor __init__
+"""
+
+
+class Base:
+    """
+    defines class Base
+    Class Attributes:
+        __nb_objects
+    Methods:
+        __init__(self, id=None)
+    """
+    __nb_ojects = 0
+
+    def __init__(self, id=None):
+        """Initialize id, increment class attribute if no id and set as id"""
+        if id in not None:
+            self.id = id
+        else:
+            Base.__nb_ojects += 1
+            self.id = Base.__nb_objects
