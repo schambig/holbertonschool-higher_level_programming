da1d307665e3a67c2fa98a8574219c1ed3b5d86e
Salomon Chambi <salomonchambi@gmail.com>
2022-10-09 18:07:50 -0700

    Update `Base`, add static method, dictionary to JSON string

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -33,6 +33,6 @@ class Base:
     def to_json_string(list_dictionaries):
         """ list of dictionaries to JSON string """
         if list_dictionaries is None or list_dictionaries == "[]":
-           return "[]"
+            return "[]"
         else:
             return json.dumps(list_dictionaries)
