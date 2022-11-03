a1f5fcf755e1fdfb8b584a4dd4c7618954d08374
Salomon Chambi <salomonchambi@gmail.com>
2022-11-03 14:16:54 -0700

    Create table `first_table` in `hbtn_0c_0` database in mysql server

--- SQL_introduction/4-first_table.sql
+++ SQL_introduction/4-first_table.sql
@@ -2,5 +2,5 @@
 -- Create a table called first_table in the current database in MySQL server
 CREATE TABLE IF NOT EXISTS first_table(
     id INT,
-    name VARCHAR(256),
-    PRIMARY KEY(id));
+    name VARCHAR(256));
+--    PRIMARY KEY(id));
