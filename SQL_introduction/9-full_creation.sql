933f27f2783987d8e0598e12c0223a20832a9e65
Salomon Chambi <salomonchambi@gmail.com>
2022-11-03 16:02:56 -0700

    Create `second_table` in `hbtn_0c_0` db, and add multiples rows

---
+++ SQL_introduction/9-full_creation.sql
@@ -0,0 +1,14 @@
+-- Create second_table in hbtn_0c_0 database in MySQL server and add multiples rows
+CREATE TABLE IF NOT EXISTS second_table (
+    id INT,
+    name VARCHAR(256),
+    score INT
+);
+-- We can also insert each register at a time or all at once (like we do here)
+INSERT INTO
+    second_table (id, name, score)
+VALUES
+    (1, 'Jhon', 10),
+    (2, 'Alex', 3),
+    (3, 'Bob', 14),
+    (4, 'George', 8);
