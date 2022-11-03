30172041f71117de8469dd7affe2014fb4b88571
Salomon Chambi <salomonchambi@gmail.com>
2022-11-03 10:51:14 -0700

    Print full description of table `first_table` from `hbtn_0c_0` db

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -1,2 +1,5 @@
 -- Print the full description of table first_table from database hbtn_0c_0 in MySQL server
-SHOW COLUMNS FROM first_table;
+-- We can also use \g or \G instead of ';' command terminator
+    -- \g  go  Send command to mysql server
+    -- \G  ego Send command to mysql server, display result vertically
+SHOW CREATE TABLE first_table;
