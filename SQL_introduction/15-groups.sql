0aaa4d1cf403345b4bcb913fd4e43b4e64af6440
Salomon Chambi <salomonchambi@gmail.com>
2022-11-04 00:41:13 -0700

    List number of rows with same score in `second_table` of `hbtn_0c_0` db

--- SQL_introduction/15-groups.sql
+++ SQL_introduction/15-groups.sql
@@ -1,2 +1,2 @@
--- List number of records with same score in second_table of the hbtn_0c_0 db in MySQL server
+-- List number of records with same score in second_table of hbtn_0c_0 db in MySQL server
 SELECT score, COUNT(*) AS number FROM second_table GROUP BY score ORDER BY number DESC;
