9eab2322af129592fd6672cc15ee78e04a493865
Salomon Chambi <salomonchambi@gmail.com>
2022-11-09 17:03:10 -0800

    List all privileges of `user_0d_1` and `user_0d_2` on localhost server

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,3 +1,3 @@
---  List all privileges of user_0d_1 and user_0d_2 on localhost server
+--  List all privileges of user_0d_1 and user_0d_2 on localhost server.
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
 SHOW GRANTS FOR 'user_0d_2'@'localhost';
