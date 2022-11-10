0f690c2ffa17065bba04093156b6d4f54c9d2e46
Salomon Chambi <salomonchambi@gmail.com>
2022-11-09 17:03:48 -0800

    Create the mysql server user `user_0d_1`

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,3 +1,3 @@
--- Create the mysql server user user_0d_1
+-- Create the mysql server user user_0d_1.
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
 GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
