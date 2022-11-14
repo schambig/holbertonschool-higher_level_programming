9da01a52564917c9813f85a1e6462e98a21ce808
Salomon Chambi <salomonchambi@gmail.com>
2022-11-13 20:38:39 -0800

    Delete `State` objects containing `a` in the name from `hbtn_0e_6_usa`

--- python-object_relational_mapping/13-model_state_delete_a.py
+++ python-object_relational_mapping/13-model_state_delete_a.py
@@ -12,9 +12,7 @@ if __name__ == '__main__':
                            pool_pre_ping=True)
 
     Session = sessionmaker(bind=engine)
-    session = Session()
-
-    for states in session.query(State):
-        if 'a' in states.name:
-            session.delete(states)
-    session.commit()
+    with Session() as session:
+        session.query(State).filter(State.name.like('%a%'))\
+            .delete(synchronize_session=False)
+        session.commit()    
