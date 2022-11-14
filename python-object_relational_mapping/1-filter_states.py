4c2e97cfaa00c65cef1ee060bdbedc6cf89739a2
Salomon Chambi <salomonchambi@gmail.com>
2022-11-13 16:51:27 -0800

    List all `states`, `name` starting with `N` from `hbtn_0e_0_usa` db

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -8,7 +8,7 @@ if __name__ == "__main__":
     db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3],
                          host='localhost', port=3306)
     cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY states.id")
-    [print(state) for state in cur.fetchall()]
+    cur.execute("SELECT * FROM states ORDER BY states.id")
+    [print(state) for state in cur.fetchall() if state[1][0] == 'N']
     cur.close()
     db.close()
