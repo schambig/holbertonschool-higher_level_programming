c0377dfc59aaafe85a9f5b22037d7e6659ff154e
Salomon Chambi <salomonchambi@gmail.com>
2022-12-11 18:17:16 -0500

    feat: export a named function that adds 2 integers

---
+++ javascript-warm_up/13-add.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+/*
+exporting a named function:
+http://51elliot.blogspot.com/2012/01/simple-intro-to-nodejs-module-scope.html
+*/
+exports.add = function add (a, b) {
+  return a + b;
+};
