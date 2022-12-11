19dc3cd0e254a3d64e494b5cb918dc1dfa7b9ad4
Salomon Chambi <salomonchambi@gmail.com>
2022-12-11 15:14:08 -0500

    refactor: keep the function simple, print the result outside of it

--- javascript-warm_up/9-add.js
+++ javascript-warm_up/9-add.js
@@ -1,8 +1,6 @@
 #!/usr/bin/node
-const a = parseInt(process.argv[2]);
-const b = parseInt(process.argv[3]);
 function add (a, b) {
-  console.log(a + b);
+  return a + b;
 }
-
-add(a, b);
+// Number() method returns NaN if the argument cannot be converted to a number
+console.log(add(Number(process.argv[2]), Number(process.argv[3])));
