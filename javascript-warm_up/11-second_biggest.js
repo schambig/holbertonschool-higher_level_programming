83315d9cb99526ed5f74a109a01ac34341f2781c
Salomon Chambi <salomonchambi@gmail.com>
2022-12-11 16:20:39 -0500

    fix: correct parsing when passing just one argument

--- javascript-warm_up/11-second_biggest.js
+++ javascript-warm_up/11-second_biggest.js
@@ -1,7 +1,8 @@
 #!/usr/bin/node
-if (isNaN(process.argv[2]) || parseInt(process.argv[2]) === 1) {
+if (isNaN(process.argv[2]) || process.argv.length === 3) {
   console.log('0');
 } else {
-  const secBiggest = process.argv.slice(2).sort((a, b) => (b - a))[1];
+  // `sort((a, b) => b - a)` is the same as `sort(function(a ,b){return b -a})`
+  const secBiggest = process.argv.slice(2).sort((a, b) => b - a)[1];
   console.log(secBiggest);
 }
