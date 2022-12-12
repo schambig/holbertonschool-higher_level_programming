a6d79f48919da0a5304e959c0067cc2e91247f24
Salomon Chambi <salomonchambi@gmail.com>
2022-12-12 17:20:24 -0500

    feat: add comment describing the code

--- javascript_objects_scopes_closures/7-occurrences.js
+++ javascript_objects_scopes_closures/7-occurrences.js
@@ -1,4 +1,5 @@
 #!/usr/bin/node
 exports.nbOccurences = function (list, searchElement) {
+  // filter return an array of the same element, use arrow function syntax
   return list.filter(element => element === searchElement).length;
 };
