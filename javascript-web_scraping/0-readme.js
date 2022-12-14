b7f204d343ac3ebcacad94f4f9ce5de1cd81cb12
Salomon Chambi <salomonchambi@gmail.com>
2022-12-14 00:51:42 -0500

    feat: add comments to clarify the code

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -1,5 +1,12 @@
 #!/usr/bin/node
 const fs = require('fs');
-fs.readFile(process.argv[2], 'utf-8', function (error, content) {
+/*
+first argument in readFile function: path or the file you want to read
+second argument: optional object in this case encoding utf-8
+third argument: a callback function executed when the file is read,
+  this callback function has two arguments: an error object and the
+  data from teh file
+*/
+fs.readFile(process.argv[2], 'utf-8', (error, content) => {
   console.log(error || content);
 });
