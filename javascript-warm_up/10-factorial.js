7efec6fe5993c2f93eea666dc3f5b1e878db1a05
Salomon Chambi <salomonchambi@gmail.com>
2024-01-11 09:00:00 -0500

    refactor: make an idiomatic js function

--- javascript-warm_up/10-factorial.js
+++ javascript-warm_up/10-factorial.js
@@ -1,5 +1,15 @@
 #!/usr/bin/node
+// function factorial (n) {
+//   return n === 0 || isNaN(n) ? 1 : n * factorial(n - 1);
+// }
+// console.log(factorial(Number(process.argv[2])));
+
 function factorial (n) {
-  return n === 0 || isNaN(n) ? 1 : n * factorial(n - 1);
+  return (n === 0 || Number.isNaN(n)) ? 1 : n * factorial(n - 1);
 }
-console.log(factorial(Number(process.argv[2])));
+// const arg = parseInt(process.argv[2])
+// const arg = Number(process.argv[2])
+const arg = +process.argv[2]; // using unary operator '+'
+const result = factorial(arg);
+
+console.log(result);
