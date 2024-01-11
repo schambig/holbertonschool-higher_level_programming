e1568525302557e0b8f97dd8e7537b395d2bb8a1
Salomon Chambi <salomonchambi@gmail.com>
2024-01-11 09:00:00 -0500

    refactor: use arrow function

--- javascript-warm_up/13-add.js
+++ javascript-warm_up/13-add.js
@@ -3,6 +3,9 @@
 exporting a named function:
 http://51elliot.blogspot.com/2012/01/simple-intro-to-nodejs-module-scope.html
 */
-exports.add = function add (a, b) {
-  return a + b;
-};
+// exports.add = function add (a, b) {
+//   return a + b;
+// };
+
+const add = (a, b) => a + b;
+module.exports = { add };
