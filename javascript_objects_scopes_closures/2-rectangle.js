778515ccfdba5b90ca50c5dfe4657df2b7a31bdb
Salomon Chambi <salomonchambi@gmail.com>
2024-01-17 09:00:00 -0500

    refactor: improve conditional statement

--- javascript_objects_scopes_closures/2-rectangle.js
+++ javascript_objects_scopes_closures/2-rectangle.js
@@ -1,11 +1,24 @@
 #!/usr/bin/node
-module.exports = class Rectangle {
+/* module.exports = class Rectangle {
   constructor (w, h) {
     if (w > 0 && h > 0) {
       // the 2 lines bellow also can be represented by:
       // [this.width, this.height] = [w, h] (one liner)
       this.width = w;
       this.height = h;
+    }
+  }
+};
+*/
+
+module.exports = class Rectangle {
+  constructor (w, h) {
+    if ((h <= 0 || h === undefined) || (w <= 0 || w === undefined)) {
+      
+    } else {
+      this.width = w;
+      this.height = h;
+      // also [this.width, this.height] = [w, h]
     }
   }
 };
