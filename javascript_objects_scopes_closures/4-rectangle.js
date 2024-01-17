10e921528e7ddeb2d052b9a16b01019e41db4995
Salomon Chambi <salomonchambi@gmail.com>
2024-01-17 09:00:00 -0500

    refactor: improve conditional and print, double and rotate  methods

--- javascript_objects_scopes_closures/4-rectangle.js
+++ javascript_objects_scopes_closures/4-rectangle.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-module.exports = class Rectangle {
+/* module.exports = class Rectangle {
   constructor (w, h) {
     if (w > 0 && h > 0) { [this.width, this.height] = [w, h]; }
   }
@@ -9,10 +9,41 @@ module.exports = class Rectangle {
   }
 
   rotate () {
+    // Using destructuring assigment (no need of a temp variable)
     [this.width, this.height] = [this.height, this.width];
   }
 
   double () {
     [this.width, this.height] = [this.width * 2, this.height * 2];
+  }
+}; */
+
+module.exports = class Rectangle {
+  constructor (w, h) {
+    if ((h <= 0 || h === undefined) || (w <= 0 || w === undefined)) {
+      // empty object
+    } else {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    for (let i = 0; i < this.height; ++i) {
+      console.log('X'.repeat(this.width));
+    }
+  }
+
+  double () {
+    this.width *= 2;
+    this.height *= 2;
+  }
+
+  rotate () {
+    // using a tempral variable (FWIW look up line 12)
+    let tmp = 0;
+    tmp = this.width;
+    this.width = this.height;
+    this.height = tmp;
   }
 };
