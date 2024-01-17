cbbc2564ea50e1c69c26b5191f88d423b8761e44
Salomon Chambi <salomonchambi@gmail.com>
2024-01-17 09:00:00 -0500

    refactor: improve conditional and print method

--- javascript_objects_scopes_closures/3-rectangle.js
+++ javascript_objects_scopes_closures/3-rectangle.js
@@ -1,10 +1,26 @@
 #!/usr/bin/node
-module.exports = class Rectangle {
+/* module.exports = class Rectangle {
   constructor (w, h) {
     if (w > 0 && h > 0) { [this.width, this.height] = [w, h]; }
   }
 
   print () {
     for (let i = 0; i < this.height; i++) console.log('X'.repeat(this.width));
+  }
+}; */
+
+module.exports = class Rectangle {
+  constructor (w, h) {
+    if ((h <= 0 || h === undefined) || (w <= 0 || w === undefined)) {
+      // empty object
+    } else { 
+      [this.width, this.height] = [w, h];
+    }
+  }
+
+  print () {
+    for (let i = 0; i < this.height; ++i) {
+      console.log('X'.repeat(this.width));
+    }
   }
 };
