e65af264e04a94da4d3cbacfcec5e006d8e29663
Salomon Chambi <salomonchambi@gmail.com>
2024-01-18 09:00:00 -0500

    refactor: reassign c's value to 'X', do not call print method

--- javascript_objects_scopes_closures/6-square.js
+++ javascript_objects_scopes_closures/6-square.js
@@ -1,10 +1,23 @@
 #!/usr/bin/node
-module.exports = class Square extends require('./5-square') {
+/* module.exports = class Square extends require('./5-square') {
   charPrint (c) {
     if (c === undefined) {
-      this.print();
+      this.print(); // use print method from Rectangle class
     } else {
       for (let i = 0; i < this.height; i++) console.log(c.repeat(this.width));
     }
   }
+}; */
+
+const SquareOne = require('./5-square');
+
+module.exports = class Square extends SquareOne {
+  charPrint (c) {
+    if (c === undefined) {
+      c = 'X'; // just assing 'X' to c variable (look up line 5)
+      }
+      for (let i = 0; i < this.height; i++) {
+        console.log(c.repeat(this.width));
+      }
+    }
 };
