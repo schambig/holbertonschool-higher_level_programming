a90cc93a51236756d26c975579a3746516e0575b
Salomon Chambi <salomonchambi@gmail.com>
2024-01-18 09:00:00 -0500

    refactor: split import and class extension

--- javascript_objects_scopes_closures/5-square.js
+++ javascript_objects_scopes_closures/5-square.js
@@ -1,5 +1,13 @@
 #!/usr/bin/node
-module.exports = class Square extends require('./4-rectangle.js') {
+/* module.exports = class Square extends require('./4-rectangle.js') {
+  constructor (size) {
+    super(size, size);
+  }
+}; */
+
+const Rectangle = require('./4-rectangle');
+
+module.exports = class Square extends Rectangle {
   constructor (size) {
     super(size, size);
   }
