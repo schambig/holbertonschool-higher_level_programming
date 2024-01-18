79b985d9dbb438b21a22f90d5dad11402b420ccb
Salomon Chambi <salomonchambi@gmail.com>
2024-01-18 09:00:00 -0500

    refactor: reassign c's value to 'X', do not call print method

--- javascript_objects_scopes_closures/6-square.js
+++ javascript_objects_scopes_closures/6-square.js
@@ -15,9 +15,9 @@ module.exports = class Square extends SquareOne {
   charPrint (c) {
     if (c === undefined) {
       c = 'X'; // just assing 'X' to c variable (look up line 5)
-      }
-      for (let i = 0; i < this.height; i++) {
-        console.log(c.repeat(this.width));
-      }
     }
+    for (let i = 0; i < this.height; i++) {
+      console.log(c.repeat(this.width));
+    }
+  }
 };
