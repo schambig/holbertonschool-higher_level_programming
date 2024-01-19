22140305cc0050ca9e06cab6a117c72e54477792
Salomon Chambi <salomonchambi@gmail.com>
2024-01-19 09:00:00 -0500

    refactor: make an idiomatic js module

--- javascript_objects_scopes_closures/7-occurrences.js
+++ javascript_objects_scopes_closures/7-occurrences.js
@@ -1,5 +1,14 @@
 #!/usr/bin/node
-exports.nbOccurences = function (list, searchElement) {
+/* exports.nbOccurences = function (list, searchElement) {
   // filter return an array of the same element, use arrow function syntax
   return list.filter(element => element === searchElement).length;
+}; */
+
+exports.nbOccurences = function (list, searchElement) {
+  let count = 0;
+  for (let i = 0; i < list.length; i++) {
+    if (searchElement === list[i]) { ++count; }
+  }
+
+  return (count);
 };
