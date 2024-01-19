6cf90e05e3770e1393ad82be36e94bc40a7504d0
Salomon Chambi <salomonchambi@gmail.com>
2024-01-19 09:00:00 -0500

    refactor: reverse a list with for loop and push method

--- javascript_objects_scopes_closures/8-esrever.js
+++ javascript_objects_scopes_closures/8-esrever.js
@@ -1,8 +1,17 @@
 #!/usr/bin/node
-exports.esrever = function (list) {
+/* exports.esrever = function (list) {
   return list.reduceRight(function (array, current) {
     array.push(current);
     return array;
   },
   []);
-};
+}; */
+
+exports.esrever = function (list) {
+  let newList = [];
+  for (let i = list.length - 1; i >= 0; i--) {
+    newList.push(list[i]);
+  }
+
+  return (newList);
+}
