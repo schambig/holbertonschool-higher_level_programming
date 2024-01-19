91f061a625d0cbdaeecd4d186bf3b1b791dc015a
Salomon Chambi <salomonchambi@gmail.com>
2024-01-19 09:00:00 -0500

    refactor: add an alternative solution

--- javascript_objects_scopes_closures/10-converter.js
+++ javascript_objects_scopes_closures/10-converter.js
@@ -1,4 +1,11 @@
 #!/usr/bin/Node
-exports.converter = function (base) {
+/* exports.converter = function (base) {
   return num => num.toString(base);
+}; */
+
+exports.converter = function (base) {
+  function num (x) {
+    return (parseInt(x, 10).toString(base));
+  }
+  return (num);
 };
