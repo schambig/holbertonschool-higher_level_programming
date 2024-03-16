0955ad9715dc87b2ee521319232884d92c01e421
Salomon Chambi <salomonchambi@gmail.com>
2024-03-16 09:00:00 -0500

    feat: update the text color when clicking

--- javascript-dom_manipulation/1-script.js
+++ javascript-dom_manipulation/1-script.js
@@ -1,15 +1,13 @@
-// Get a reference to the header element
+// Get a reference to the header and red_header elements
 const header = document.querySelector('header');
-
-// Get a reference to the "Red header" div
 const redHeader = document.getElementById('red_header');
 
 /* redHeader.onclick = () => {
   header.style.color = '#FF0000';
 }; */
 
-// Add an event listener to the "Red header" div
+// Use event listener to the "Red header" best practice)
 redHeader.addEventListener('click', function() {
-  // Change the color of the header when the "Red header" div is clicked
-  header.style.color = '#FF0000'; // Change to red
+  // Change the color (to red) of the header when the "Red header" div is clicked
+  header.style.color = '#FF0000';
 });
