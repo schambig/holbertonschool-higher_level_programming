59fdd0cf07b189e2297f89c229e42d7fdbf3acc5
Salomon Chambi <salomonchambi@gmail.com>
2024-03-27 09:00:00 -0500

    feat: fetch a character name from starwars api

---
+++ javascript-dom_manipulation/6-script.js
@@ -0,0 +1,33 @@
+const url = 'https://swapi-api.hbtn.io/api/people/13/?format=json';
+
+/* 
+// use 'old' syntax, .then()
+fetch(url)
+  .then((res) => {
+    if (!res.ok) throw new Error(`Sure about that URL? (${res.status})`);
+    return res.json();
+  })
+  .then((data) => {
+    const characterElement = document.getElementById('character');
+    let charName = data.name;
+    // characterElement.innerHTML = charName;
+    characterElement.textContent = charName;
+  });
+ */
+
+// use async-await, modern syntax
+async function displayCharacter() {
+  try {
+    const res = await fetch(url);
+    console.log(res)
+    if (!res.ok) throw new Error(`Sure about that URL? (${res.status})`);
+    // use object destructuring to extract the name property from the object returned by res.json()
+    const { name } = await res.json();
+    // document.getElementById('character').innerHTML = name;
+    document.getElementById('character').textContent = name;
+  } catch (e) {
+    console.error('Error:', e);
+  }
+}
+
+displayCharacter();
