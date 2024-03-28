677a414454f12030f3a4aa30a2920bdedf78f7ed
Salomon Chambi <salomonchambi@gmail.com>
2024-03-28 09:00:00 -0500

    feat: fetch a character name from starwars api

--- javascript-dom_manipulation/6-script.js
+++ javascript-dom_manipulation/6-script.js
@@ -26,8 +26,8 @@ async function displayCharacter() {
     // document.getElementById('character').innerHTML = name;
     document.getElementById('character').textContent = name;
     // line above can be done in two lines (as follows) for more readability:
-    // const characterName = document.getElementById('character')
-    // characterName.textContent = name;
+    // const characterElement = document.getElementById('character')
+    // characterElement.textContent = name;
   } catch (e) {
     console.error('Error:', e);
   }
