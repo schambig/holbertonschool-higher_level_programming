238d1cdf6bd3a0fb32739dc42573760b938cef4a
Salomon Chambi <salomonchambi@gmail.com>
2024-03-29 09:00:00 -0500

    feat: fetch a character name from starwars api

--- javascript-dom_manipulation/6-script.js
+++ javascript-dom_manipulation/6-script.js
@@ -19,7 +19,7 @@ fetch(url)
 async function displayCharacter() {
   try {
     const res = await fetch(url);
-    console.log(res)
+    // console.log(res)
     if (!res.ok) throw new Error(`Sure about that URL? (${res.status})`);
     // use object destructuring to extract the name property from the object returned by res.json()
     const { name } = await res.json();
